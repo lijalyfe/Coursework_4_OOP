76bee57868c715313c56799489080e5845bc455a
lija_lyfe <lija6556@list.ru>
2023-06-26 03:32:50 +0500

    add class JSONSever

---
+++ src/JSONSever.py
@@ -0,0 +1,13 @@
+import json
+
+
+class JSONSaver:
+    def __init__(self):
+        self.data = []
+
+    def add_vacancy(self, vacancy: dict):
+        self.data.append(vacancy)
+
+    def save_data(self, file_name: str):
+        with open(file_name, "w", encoding="utf-8") as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=4)
