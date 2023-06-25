08430214122941a7d2adc7750a83dc1b41bbd95e
lija_lyfe <lija6556@list.ru>
2023-06-26 03:56:43 +0500

    renamed from JSONSaver.py

--- src/JSONSaver.py
+++ src/JSONSaver.py
@@ -11,4 +11,3 @@ class JSONSaver:
     def save_data(self, file_name: str):
         with open(file_name, "w", encoding="utf-8") as f:
             json.dump(self.data, f, ensure_ascii=False, indent=4)
-
