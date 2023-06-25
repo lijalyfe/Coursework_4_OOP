3faa604597b55d1ac59cac9c6bf7693cd7c1064c
lija_lyfe <lija6556@list.ru>
2023-06-26 03:10:43 +0500

    add class HeadHunterAPI

--- src/main.py
+++ src/main.py
@@ -2,3 +2,13 @@ import request
 from typing import List
 
 
+class HeadHunterAPI:
+    def __init__(self):
+        self.base_url = "https://api.hh.ru"
+
+    def get_vacancies(self, search_query: str) -> List[dict]:
+        vacancies_url = f"{self.base_url}/vacancies"
+        params = {'text': search_query}
+        response = requests.get(vacancies_url, params=params)
+        response.raise_for_status()
+        return response.json()['items']
