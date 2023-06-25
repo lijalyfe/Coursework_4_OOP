5a14458c57c1564d285998fa90a0648854e35d88
lija_lyfe <lija6556@list.ru>
2023-06-26 03:29:35 +0500

    add def user_interaction

--- src/main.py
+++ src/main.py
@@ -46,3 +46,33 @@ class SuperJobAPI:
         for vacancy in vacancies:
             print(f"{vacancy['name']}\n{vacancy['employer']['name']}\n{vacancy['alternate_url']}\n{'-' * 30}\n")
 
+    def user_interaction(self):
+        from JSONSaver import JSONSaver
+
+        hh_api = HeadHunterAPI()
+        app_key = "v3.r.137636772.89b8dc4297218a5dcf9c8e6f468e738f0182aae0.5d001ac5ff302dddcf98f5f8de641b4d947f0c4e"
+        sj_api = SuperJobAPI(app_key)
+
+        search_query = input("Введите поисковый запрос: ")
+        top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+        filter_words = input("Введите ключевые слова для фильтрации вакансий: ").lower().split()
+
+        hh_vacancies = hh_api.get_vacancies(search_query)
+        sj_vacancies = []
+
+        filtered_vacancies = SuperJobAPI.filter_vacancies(hh_vacancies, sj_vacancies, filter_words)
+        if not filtered_vacancies:
+            print("Нет вакансий, соответствующих заданным критериям.")
+        sorted_vacancies = SuperJobAPI.sort_vacancies(filtered_vacancies)
+        top_vacancies = SuperJobAPI.get_top_vacancies(sorted_vacancies, top_n)
+
+        SuperJobAPI.print_vacancies(sj_api, top_vacancies)
+
+        save_to_file = input("Сохранить вакансии в файл? (y/n): ")
+        if save_to_file.lower() == "y":
+            json_saver = JSONSaver()
+            for vacancy in filtered_vacancies:
+                json_saver.add_vacancy(vacancy)
+            json_saver.save_data("vacancies.json")
+            print("Вакансии сохранены в файл.")
+
